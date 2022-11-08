42a3d3b5f2f78fe80311d11dd16be9c2ccbcf5a5
yantingwei <akkoyan@qq.com>
2022-11-08 17:05:41 +0800

    :speech_balloon: 修改框架信息

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,28 +1,28 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = 'Design by Akko';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'Akko',
+          title: 'Akko Space',
+          href: 'https://akko.space',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/AkkoSpace',
           blankTarget: true,
         },
         {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          key: 'AkkoGitea' ,
+          title: 'Akko\'s Gitea',
+          href: 'https://git.akko.space',
           blankTarget: true,
         },
       ]}
