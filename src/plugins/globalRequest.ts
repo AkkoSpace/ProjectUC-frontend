4adf3577d82b1a5841059915996c3773f47b1609
yantingwei <akkoyan@qq.com>
2022-11-10 16:12:44 +0800

    :necktie: 全局响应拦截器

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -12,7 +12,7 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV === 'production' ? 'https://uc.api.akko.space' : undefined,
+  prefix: process.env.NODE_ENV === 'production' ? 'https://api.uc.akko.space' : undefined,
   // requestType: 'form',
 });
 
