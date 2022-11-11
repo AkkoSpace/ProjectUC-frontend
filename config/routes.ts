f36c3b069877cbef2e3bfd406940d00095e6f67c
yantingwei <akkoyan@qq.com>
2022-11-11 11:21:37 +0800

    :necktie: 修改路由

--- config/routes.ts
+++ config/routes.ts
@@ -3,23 +3,49 @@
     path: '/user',
     layout: false,
     routes: [
-      {name: '登录', path: '/user/login', component: './user/Login'},
-      {name: '注册', path: '/user/register', component: './user/Register'},
-      {component: './404'},
+      {
+        name: '登录',
+        path: '/user/login',
+        component: './user/Login',
+      },
+      {
+        name: '注册',
+        path: '/user/register',
+        component: './user/Register',
+      },
+      {
+        component: './404',
+      },
     ],
   },
-  {path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome'},
+  {
+    path: '/welcome',
+    name: '欢迎',
+    icon: 'smile',
+    component: './Welcome',
+  },
   {
     path: '/admin',
     name: '管理页',
     icon: 'crown',
     access: 'canAdmin',
     routes: [
-      {path: '/admin/sub-page', name: '用户管理', icon: 'smile', component: './Admin/UserManage'},
-      {component: './404'},
+      {
+        path: '/admin/sub-page',
+        name: '用户管理',
+        icon: 'smile',
+        component: './Admin/UserManage',
+      },
+      {
+        component: './404',
+      },
     ],
   },
-  {name: '查询表格', icon: 'table', path: '/list', component: './TableList'},
-  {path: '/', redirect: '/welcome'},
-  {component: './404'},
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
 ];
